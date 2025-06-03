dd47cd5c5b66425878420691a68e4cd1fee5cdfa
EC2 Default User <ec2-user@ip-172-31-4-194.ap-northeast-2.compute.internal>
2025-06-03 07:49:55 +0000

    init.tf파일 통합 후 dynamodb 생성

--- init.tf
+++ init.tf
@@ -2,7 +2,17 @@ provider "aws" {
      region = "ap-northeast-2"
 }
 
-resource "aws_s3_bucket" "s3" {
+resource "aws_s3_bucket" "tfstate" {
      bucket = "terraform-git-whdbtjd"
 }
 
+resource "aws_dynamodb_table" "terraform_state_lock" {
+     name          = "terraform-lock-whdbtjd"
+     hash_key      = "LockID" # 락기능 사용하려면 이 속성값이 필수, attribute도 마찬가지
+     billing_mode   = "PAY_PER_REQUEST"
+
+     attribute {
+       name = "LockID"
+       type = "S"
+     }
+}
