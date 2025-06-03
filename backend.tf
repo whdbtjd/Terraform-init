45433bd3dfe5bbf6f5e4cf0d2027559afc13a8bb
EC2 Default User <ec2-user@ip-172-31-4-194.ap-northeast-2.compute.internal>
2025-06-03 07:20:18 +0000

    backend.tf 삭제

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-     backend "s3" {
-          bucket          = "Terraform-git-bucket"
-          key             = "/git" # s3에 저장되는 경로
-          region          = "ap-northeast-2"
-          encrypt         = true
-          dynamodb_table  = "terraform-lock"
-}
