e001da38f8acfa56aaca15e7b4c0f8a75aa72d0f
Jo Yu Sung <cys990617@gmail.com>
2025-06-03 11:43:19 +0000

    user 리소스 생성 완료

--- iam.tf
+++ iam.tf
@@ -6,3 +6,10 @@ resource "aws_iam_user" "dev_team_staff" {
       name = "dev_team_staff"
 }
 
+resource "aws_iam_user" "ops_team_leader" {
+      name = "ops_team_leader"
+}
+
+resource "aws_iam_user" "ops_team_staff" {
+      name = "ops_team_staff"
+}
