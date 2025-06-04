fcd64133167046715ba3be64714fc4c73340f460
Jo Yu Sung <cys990617@gmail.com>
2025-06-04 10:31:17 +0000

    private 서브넷 및 라우팅 테이블 어소시에이션

--- vpc.tf
+++ vpc.tf
@@ -62,3 +62,11 @@ resource "aws_route_table_association" "public" {
 
      subnet_id      = aws_subnet.public[count.index].id
 }
+
+resource "aws_route_table_association" "private" {
+     count          = length(var.aws_vpc_private)
+
+     route_table_id = aws_route_table.private[count.index].id
+
+     subnet_id      = aws_subnet.private[count.index].id
+}
