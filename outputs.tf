1290573d974a4a0090d62018ca9e5987d069cc0c
Jo Yu Sung <cys990617@gmail.com>
2025-06-04 06:49:52 +0000

    outputs.tf에 서브넷 변수 추가

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@ output "dev_team_user" {
 output "ops_team_user" {
      value = var.ops_team_users
 }
+
+output "aws_vpc_public" {
+     value = var.aws_vpc_public
+}
+
+output "aws_vpc_private" {
+     value = var.aws_vpc_private
+}
