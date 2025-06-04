7d1637394e038048082f451d8fbe694e09d70086
Jo Yu Sung <cys990617@gmail.com>
2025-06-04 09:44:04 +0000

    private서브넷 전용 라우팅 테이블 2개 생성

--- vpc.tf
+++ vpc.tf
@@ -41,3 +41,12 @@ resource "aws_nat_gateway" "nat" {
      subnet_id         = aws_subnet.public[count.index].id
 }
 
+resource "aws_route_table" "private" {
+     vpc_id = aws_vpc.main.id
+     count = 2
+
+     route {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = aws_nat_gateway.nat[count.index].id
+      }
+}
