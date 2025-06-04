ee0d67cf2f3d152770d7658fb13b8d31d63be27b
Jo Yu Sung <cys990617@gmail.com>
2025-06-04 07:24:41 +0000

    퍼블릭 서브넷 리소스 생성

--- vpc.tf
+++ vpc.tf
@@ -2,4 +2,9 @@ resource "aws_vpc" "main" {
      cidr_block = "10.0.0.0/16"
 }
 
+resource "aws_subnet" "public" {
+  count = length(var.aws_vpc_public)
+  vpc_id = aws_vpc.main.id
 
+  cidr_block = element(var.aws_vpc_public, count.index)
+}
