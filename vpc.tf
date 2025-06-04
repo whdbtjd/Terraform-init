7a45e40b7eb2b4075617050751a76db2f138c673
Jo Yu Sung <cys990617@gmail.com>
2025-06-04 07:28:40 +0000

    프라이빗 서브넷 생성

--- vpc.tf
+++ vpc.tf
@@ -3,8 +3,16 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "public" {
-  count = length(var.aws_vpc_public)
-  vpc_id = aws_vpc.main.id
+     count = length(var.aws_vpc_public)
+     vpc_id = aws_vpc.main.id
 
-  cidr_block = element(var.aws_vpc_public, count.index)
+     cidr_block = element(var.aws_vpc_public, count.index)
 }
+
+resource "aws_subnet" "private" {
+     count = length(var.aws_vpc_private)
+     vpc_id = aws_vpc.main.id
+
+     cidr_block = element(var.aws_vpc_private, count.index)
+}
+  
