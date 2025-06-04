f665603b2ad31176fac3e104b364263e6a940b0a
Jo Yu Sung <cys990617@gmail.com>
2025-06-04 09:12:43 +0000

    public route table 리소스 생성

--- vpc.tf
+++ vpc.tf
@@ -15,4 +15,17 @@ resource "aws_subnet" "private" {
 
      cidr_block = element(var.aws_vpc_private, count.index)
 }
+
+resource "aws_internet_gateway" "igw" {
+     vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public" {
+     vpc_id = aws_vpc.main.id
+
+     route {
+       cidr_block = "0.0.0.0/0"
+       gateway_id = aws_internet_gateway.igw.id
+     }
+}
   
